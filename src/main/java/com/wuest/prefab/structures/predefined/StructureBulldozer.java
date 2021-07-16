e5cea5a4c4eb3a775e5845ba439f915c5ccb86ef
Brian Wuest <brian.wuest@outlook.com>
2021-07-16 18:11:55 -0500

    Fixing issue where the bulldozer wasn't dropping blocks

--- src/main/java/com/wuest/prefab/structures/predefined/StructureBulldozer.java
+++ src/main/java/com/wuest/prefab/structures/predefined/StructureBulldozer.java
@@ -13,6 +13,7 @@ import net.minecraft.block.FluidBlock;
 import net.minecraft.entity.decoration.AbstractDecorationEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.MiningToolItem;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 
@@ -23,56 +24,73 @@ import java.util.ArrayList;
  */
 public class StructureBulldozer extends Structure {
 
-	/**
-	 * Initializes a new instance of the {@link StructureBulldozer} class.
-	 */
-	public StructureBulldozer() {
-		BuildClear clearedSpace = new BuildClear();
-		clearedSpace.getShape().setDirection(Direction.SOUTH);
-		clearedSpace.getShape().setHeight(15);
-		clearedSpace.getShape().setLength(16);
-		clearedSpace.getShape().setWidth(16);
-		clearedSpace.getStartingPosition().setSouthOffset(1);
-		clearedSpace.getStartingPosition().setEastOffset(8);
-		clearedSpace.getStartingPosition().setHeightOffset(1);
+    protected static MiningToolItem diamondPickaxe;
+    protected static MiningToolItem diamondShovel;
+    protected static MiningToolItem diamondAxe;
+    protected static ItemStack diamondPickaxeStack;
+    protected static ItemStack diamondShovelStack;
+    protected static ItemStack diamondAxeStack;
 
-		this.setClearSpace(clearedSpace);
-		this.setBlocks(new ArrayList<>());
-	}
+    static {
+        StructureBulldozer.diamondAxe = (MiningToolItem) Items.DIAMOND_AXE;
+        StructureBulldozer.diamondPickaxe = (MiningToolItem) Items.DIAMOND_PICKAXE;
+        StructureBulldozer.diamondShovel = (MiningToolItem) Items.DIAMOND_SHOVEL;
+        StructureBulldozer.diamondPickaxeStack = new ItemStack(Items.DIAMOND_PICKAXE);
+        StructureBulldozer.diamondShovelStack = new ItemStack(Items.DIAMOND_SHOVEL);
+        StructureBulldozer.diamondAxeStack = new ItemStack(Items.DIAMOND_AXE);
+    }
 
-	/**
-	 * This method is to process before a clear space block is set to air.
-	 *
-	 * @param pos The block position being processed.
-	 */
-	@Override
-	public void BeforeClearSpaceBlockReplaced(BlockPos pos) {
-		BlockState state = this.world.getBlockState(pos);
-		BulldozerConfiguration configuration = (BulldozerConfiguration) this.configuration;
+    /**
+     * Initializes a new instance of the {@link StructureBulldozer} class.
+     */
+    public StructureBulldozer() {
+        BuildClear clearedSpace = new BuildClear();
+        clearedSpace.getShape().setDirection(Direction.SOUTH);
+        clearedSpace.getShape().setHeight(15);
+        clearedSpace.getShape().setLength(16);
+        clearedSpace.getShape().setWidth(16);
+        clearedSpace.getStartingPosition().setSouthOffset(1);
+        clearedSpace.getStartingPosition().setEastOffset(8);
+        clearedSpace.getStartingPosition().setHeightOffset(1);
 
-		// Only harvest up to diamond level and non-indestructable blocks.
-		//Tag<Item> blah = new Tag.Identified<Item>();
-		//ToolManagerImpl.entry(state.getBlock()).getMiningLevel()
-		boolean pickAxeEffective = ToolManagerImpl.handleIsEffectiveOnIgnoresVanilla(state, new ItemStack(Items.DIAMOND_PICKAXE), null, false);
-		boolean axeEffective = ToolManagerImpl.handleIsEffectiveOnIgnoresVanilla(state, new ItemStack(Items.DIAMOND_AXE), null, false);
-		boolean shovelEffective = ToolManagerImpl.handleIsEffectiveOnIgnoresVanilla(state, new ItemStack(Items.DIAMOND_SHOVEL), null, false);
+        this.setClearSpace(clearedSpace);
+        this.setBlocks(new ArrayList<>());
+    }
 
-		if (!configuration.creativeMode && Prefab.serverConfiguration.allowBulldozerToCreateDrops && (pickAxeEffective || axeEffective || shovelEffective) && state.getHardness(world, pos) >= 0.0f) {
-			Block.dropStacks(state, this.world, pos);
-		}
+    /**
+     * This method is to process before a clear space block is set to air.
+     *
+     * @param pos The block position being processed.
+     */
+    @Override
+    public void BeforeClearSpaceBlockReplaced(BlockPos pos) {
+        BlockState state = this.world.getBlockState(pos);
+        BulldozerConfiguration configuration = (BulldozerConfiguration) this.configuration;
 
-		if (configuration.creativeMode && state.getBlock() instanceof FluidBlock) {
-			// This is a fluid block, replace it with stone so it can be cleared.
-			BuildingMethods.ReplaceBlock(this.world, pos, Blocks.STONE);
-		}
-	}
+        boolean pickAxeEffective = ToolManagerImpl.handleIsEffectiveOnIgnoresVanilla(state, StructureBulldozer.diamondPickaxeStack, null, StructureBulldozer.diamondPickaxe.isSuitableFor(state));
+        boolean axeEffective = ToolManagerImpl.handleIsEffectiveOnIgnoresVanilla(state, StructureBulldozer.diamondAxeStack, null, StructureBulldozer.diamondAxe.isSuitableFor(state));
+        boolean shovelEffective = ToolManagerImpl.handleIsEffectiveOnIgnoresVanilla(state, StructureBulldozer.diamondShovelStack, null, StructureBulldozer.diamondShovel.isSuitableFor(state));
 
-	@Override
-	public void BeforeHangingEntityRemoved(AbstractDecorationEntity hangingEntity) {
-		// Only generate drops for this hanging entity if the bulldozer allows it.
-		// By default the base class doesn't allow hanging entities to generate drops.
-		if (Prefab.serverConfiguration.allowBulldozerToCreateDrops) {
-			hangingEntity.onBreak(null);
-		}
-	}
+        if (!configuration.creativeMode &&
+                Prefab.serverConfiguration.allowBulldozerToCreateDrops
+                && ((state.isToolRequired() && pickAxeEffective || axeEffective || shovelEffective)
+                || !state.isToolRequired())
+                && state.getHardness(world, pos) >= 0.0f) {
+            Block.dropStacks(state, this.world, pos);
+        }
+
+        if (configuration.creativeMode && state.getBlock() instanceof FluidBlock) {
+            // This is a fluid block, replace it with stone so it can be cleared.
+            BuildingMethods.ReplaceBlock(this.world, pos, Blocks.STONE);
+        }
+    }
+
+    @Override
+    public void BeforeHangingEntityRemoved(AbstractDecorationEntity hangingEntity) {
+        // Only generate drops for this hanging entity if the bulldozer allows it.
+        // By default the base class doesn't allow hanging entities to generate drops.
+        if (Prefab.serverConfiguration.allowBulldozerToCreateDrops) {
+            hangingEntity.onBreak(null);
+        }
+    }
 }
