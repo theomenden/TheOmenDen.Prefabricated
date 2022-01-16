7c15e1a4939b065284f2d0e2cf31eb7adf32108a
Brian Wuest <brian.wuest@outlook.com>
2022-01-16 10:26:41 -0600

    Adding restaurant and juice shop

--- src/main/java/com/wuest/prefab/structures/config/enums/ModernBuildingsOptions.java
+++ src/main/java/com/wuest/prefab/structures/config/enums/ModernBuildingsOptions.java
@@ -92,6 +92,20 @@ public class ModernBuildingsOptions extends BaseOption{
             true,
             true);
 
+    public static ModernBuildingsOptions Restaurant = new ModernBuildingsOptions(
+            "prefab.gui.item_modern_restaurant",
+            "assets/prefab/structures/modern_restaurant.zip",
+            "textures/gui/modern_restaurant_topdown.png",
+            true,
+            true);
+
+    public static ModernBuildingsOptions JuiceShop = new ModernBuildingsOptions(
+            "prefab.gui.item_modern_juice_shop",
+            "assets/prefab/structures/modern_juice_shop.zip",
+            "textures/gui/modern_juice_shop_topdown.png",
+            true,
+            true);
+
     protected ModernBuildingsOptions(String translationString,
                           String assetLocation,
                           String pictureLocation,
